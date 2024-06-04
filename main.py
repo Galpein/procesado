8ffd0dcbf07616180804d308e3e96d3b8bdae3a0
Galpein <117558192+Galpein@users.noreply.github.com>
2024-06-04 19:05:20 +0200

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import music21  # Librería para manejar archivos musicales
-
-def generar_musica():
-    # Aquí irá la lógica para generar música
-    print("Generando música...")
-
-if __name__ == "__main__":
-    generar_musica()
